c9eebc6a15dc08efbe11fdcd01133db36df9e7ba
Chad Magendanz <chad@magendanz.com>
2017-07-19 16:06:13 -0700

    Some clean-up; q20x build now functional (although with video sync issues)

--- p200/recovery/root/sbin/postrecoveryboot.sh
+++
@@ -1,5 +0,0 @@
-#!/sbin/sh
-if [ -z $(lsmod | grep "dwc") ]; then
-  insmod /boot/aml_nftl_dev.ko
-  insmod /boot/dwc_otg.ko
-fi
--- p212/recovery/root/sbin/postrecoveryboot.sh
+++
@@ -1,5 +0,0 @@
-#!/sbin/sh
-if [ -z $(lsmod | grep "dwc") ]; then
-  insmod /boot/dwc3.ko
-  insmod /boot/dwc_otg.ko
-fi
